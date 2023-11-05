f1c30b8adf5172a4a94f4851594036e47b4d583e
alihaghayegh <haghayeghali95@gmail.com>
2023-11-05 19:50:35 +0330

    searching added to item/views and urls and base.html

--- onlineShop/item/views.py
+++ onlineShop/item/views.py
@@ -5,6 +5,14 @@ from .forms import NewItemFrom, EditItemFrom
 from .models import Item
 
 
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+
+    return render(request, 'item/items.html', {
+        'items': items,
+    })
+
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[:3]
